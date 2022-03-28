1f4d5cd68773590c0dd8033c656a47b4fe489a35
Adam Brzakala <adam.brz@onet.eu>
2022-03-28 18:53:10 +0200

    Add history widget, update layout

--- widgets/file_upload_layout.py
+++ widgets/file_upload_layout.py
@@ -6,6 +6,7 @@ from widgets.clients_queue import ClientsQueue
 from widgets.client import Client
 from widgets.folders import Folders
 from widgets.folder import Folder
+from widgets.history import History
 
 Builder.load_file("./widgets/kv/file_upload_layout.kv")
 
---
+++ widgets/history.py
@@ -0,0 +1,7 @@
+from kivy.lang.builder import Builder
+from kivy.uix.floatlayout import FloatLayout
+
+Builder.load_file("./widgets/kv/history.kv")
+
+class History(FloatLayout):
+    pass
